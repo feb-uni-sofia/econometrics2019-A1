53e957b0c0f6eb050243cbea3eab76ac875e37ad
Boyko Amarov <boyko.amarov@gmail.com>
2019-03-06 13:15:41 +0200

    Update Problem_2.R

--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,22 @@
 
+## b)
+x <- c(1, 4, 5.8)
+
+## c)
+y <- c(2, 3, 8)
+
+## d)
+z <- c(x, y)
+
+## Operations with vectors 
+## (discussed in class)
+
+c(1, 2, 3, 4) + 1
+c(1, 2, 3, 4) + c(0, 1)
+c(1, 2, 3, 4) + c(0, 1, 2)
+
+## Pay attention to how vectors with
+## a shorter length are recycled.
+
+## e)
+## Ignore for now
